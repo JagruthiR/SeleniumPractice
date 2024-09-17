492c8c4ee7b9eee40e807dc40bc5830df6330409
LENOVO <LENOVO@DESKTOP-VN9QFS7>
2024-09-17 10:01:56 +0530

    This is my alerts page.

---
+++ src/test/java/practice/Alerts.java
@@ -0,0 +1,45 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Alerts {
+ 
+	public WebDriver driver;
+ 
+@Test
+ public void handlingalerts() throws Exception {
+  
+	driver.findElement(By.id("alertButton")).click();
+	String str = driver.switchTo().alert().getText();
+	System.out.println(str);
+	
+	Thread.sleep(5000);
+	
+	driver.switchTo().alert().accept();
+	
+}
+  @BeforeTest
+  public void openUrl() throws Exception {
+ 
+	  WebDriverManager.chromedriver().setup();
+	  driver = new ChromeDriver();
+	  driver.get("https://demoqa.com/alerts");
+	  driver.manage().window().maximize();
+	  Thread.sleep(4000);
+  }
+
+  @AfterTest
+  public void afterTest() {
+ 
+	  driver.quit();
+  }
+
+}
